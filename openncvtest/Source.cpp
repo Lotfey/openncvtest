30e6e2052d7a3abf2fe56539bfa5f5f243895d05
Lotfi <achirilotfi2@gmail.com>
2022-12-17 17:38:38 +0100

    Add project files.

---
+++ openncvtest/Source.cpp
@@ -0,0 +1,24 @@
+// OpenCV includes
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+
+//Functionalities includes
+#include<iostream>
+
+
+int main()
+{ 
+	// Set input data info
+	std::string  PATH_TO_IMAGES= "standard_test_images/"; 
+	std::string imageName= "cameraman.tif";
+	
+	// read images
+	cv::Mat imgOrigin = cv::imread(PATH_TO_IMAGES+imageName);
+	
+	//Display
+	imshow("original image of cameraman", imgOrigin);
+	cv::waitKey(0);
+
+	return 0;
+}
